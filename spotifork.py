97276f48a9c520aa7fb3d0db0c920fafd7b534e4
Xavier Alexander <xalexander51@gmail.com>
2021-09-13 19:49:16 -0400

    Add comment

--- spotifork.py
+++ spotifork.py
@@ -54,6 +54,7 @@ with open('album_info.json') as file:
 
 
 #Get current playlist song ids to check for duplicates
+#Offset is used for Spotify's 100 song request limit
 for i in range(0,400,100):
     playlsit_tracks_request = requests.get('https://api.spotify.com/v1/playlists/'+ PLAYLIST_ID + '/tracks?offset='+ str(i), headers=header).json()
     playlsit_tracks_results = playlsit_tracks_request['items']
